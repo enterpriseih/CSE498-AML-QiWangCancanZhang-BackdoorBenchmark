a4552ec2c52735f567823221603aba30bff339bb
wangqi <wangqiuoe@126.com>
2022-11-21 16:40:50 -0500

    update qi

---
+++ conf.py
@@ -0,0 +1,10 @@
+attacks = {
+    'Imperceptible': "Imperceptible Backdoor Attack: From Input Space to Feature Representation",
+    'BadNet': '',
+    'BppAttack':'',
+    'WaNet':''
+}
+
+defense = {
+        'channelLipschitzness': '',
+}
