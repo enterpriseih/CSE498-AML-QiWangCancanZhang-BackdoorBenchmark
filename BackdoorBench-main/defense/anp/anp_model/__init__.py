e6660d969ffff55c985bd54414a11e501224bf27
wangqi <wangqiuoe@126.com>
2022-11-22 09:01:27 -0500

    rm backdoorbench

--- BackdoorBench-main/defense/anp/anp_model/__init__.py
+++
@@ -1,6 +0,0 @@
-from .vgg_anp import *
-from .anp_batchnorm import *
-from .preact_anp import *
-from .mobilenet_anp import *
-from .eff_anp import *
-from .den_anp import *
--- BackdoorBench-main/sh/timagenet_download.sh
+++
@@ -1,4 +0,0 @@
-wget -P ../data http://cs231n.stanford.edu/tiny-imagenet-200.zip
-unzip ../data/tiny-imagenet-200.zip -d ../data;
-mv ../data/tiny-imagenet-200 ../data/tiny;
-echo "Download Completed";
