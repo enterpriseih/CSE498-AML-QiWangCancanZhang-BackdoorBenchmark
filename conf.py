40020a87fbdec17c23883fc9285e57389de0748f
wangqi <wangqiuoe@126.com>
2022-11-25 09:51:06 -0500

    update qi

--- conf.py
+++ conf.py
@@ -1,10 +1,11 @@
 attacks = {
     'Imperceptible': "Imperceptible Backdoor Attack: From Input Space to Feature Representation",
-    'BadNet': '',
-    'BppAttack':'',
-    'WaNet':''
+    'BadNet': 'Badnets: Evaluating backdooring attacks on deep neural networks',
+    'BppAttack':'BppAttack: Stealthy and Efficient Trojan Attacks against Deep Neural Networks via Image Quantization and Contrastive Adversarial Learning',
+    'WaNet':'Wanet–imperceptible warpingbased backdoor attack',
+    'ISSBA': "Invisible backdoor attack with samplespecific triggers"
 }
 
 defense = {
-        'channelLipschitzness': '',
+        'CLP': 'Data-free backdoor removal based on channel lipschitzness',
 }
