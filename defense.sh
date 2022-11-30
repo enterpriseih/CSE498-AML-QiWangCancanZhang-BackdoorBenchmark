5358d72ad748e7923fe79fed57e377da07b376e0
zhangcancan3 <zhangcancan3@jd.com>
2022-11-29 20:42:58 -0500

    add defense

---
+++ defense.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+attack=$1
+defense=$2
+dataset=$3
+device=$4
+n_iters=$5
+batch_size=128
+
+if [ "$defense" = 'neural_cleanse' ]
+then
+  python -u defense/WaNet/defenses/neural_cleanse.py --dataset $dataset --attack $attack --attack_mode all2one --data_root 'datasets/' --checkpoints "checkpoints/${attack}/"
+fi
