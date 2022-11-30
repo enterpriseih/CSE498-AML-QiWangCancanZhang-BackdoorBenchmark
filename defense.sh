cf12b2f591d828d7ef5beb83b619297330f15c9b
zhangcancan3 <zhangcancan3@jd.com>
2022-11-29 21:27:57 -0500

    update

--- defense.sh
+++ defense.sh
@@ -6,9 +6,10 @@ dataset=$3
 device=$4
 n_iters=$5
 batch_size=128
-
+cur_dir=$PWD
 if [ "$defense" = 'neural_cleanse' ]
 then
-  #cd defense/WaNet/defenses/neural_cleanse
-  python -u defense/WaNet/defenses/neural_cleanse/neural_cleanse.py --dataset $dataset --attack $attack --attack_mode all2one --data_root 'datasets/' --checkpoints "checkpoints/${attack}/" --device $device --bs $batch_size --n_iters $n_iters
+  cd defense/WaNet/defenses/neural_cleanse
+  python -u neural_cleanse.py --dataset $dataset --attack $attack --attack_mode all2one --data_root "${cur_dir}/datasets/" --checkpoints "${cur_dir}/checkpoints/${attack}/" --device $device --bs $batch_size --n_iters $n_iters
+  cd $cur_dir
 fi
