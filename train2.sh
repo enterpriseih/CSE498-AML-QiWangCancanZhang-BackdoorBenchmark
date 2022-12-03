3075410cd94f7727a6e10248427db35da2ebd531
zhangcancan3 <zhangcancan3@jd.com>
2022-12-03 09:56:47 -0500

    combine them together as model

---
+++ train2.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+attack=$1
+dataset=$2
+device=$3
+n_iters=$4
+batch_size=128
+
+if [ "$attack" = "BppAttack" ]
+then 
+    python -u model/train/$attack.py --dataset $dataset --attack_mode all2one --squeeze_num 32 --data_root 'datasets/' --checkpoints "checkpoints/${attack}/" --device $device --n_iters $n_iters --bs $batch_size
+elif [ "$attack" = "WaNet" ]
+then
+    python model/train/$attack.py --dataset $dataset --attack_mode all2one --data_root 'datasets/' --checkpoints "checkpoints/${attack}/" --device $device --n_iters $n_iters --bs $batch_size
+elif [ "$attack" = "BadNet" ]
+then
+    python model/train/$attack.py --dataset $dataset --attack_mode all2one --data_root 'datasets/' --checkpoints "checkpoints/${attack}/" --device $device --n_iters $n_iters --bs $batch_size
+elif [ "$attack" = "Clean" ]
+then
+    python model/train/$attack.py --dataset $dataset --data_root 'datasets/' --checkpoints "checkpoints/${attack}/" --device $device --n_iters $n_iters --bs $batch_size
+fi
